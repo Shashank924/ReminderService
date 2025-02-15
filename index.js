5704f05d16098b7053be29da0243ce35d27e949f
Shashank924 <shashankshekhar955@gmail.com>
2025-02-15 17:54:57 +0530

    Basic nodemailer setup completed

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./src/config/serverConfig')
+const nodeCron = require('node-cron');
 
 const setupAndStartServer = async () => {
-    const app = express();
+
+    const app = express();   
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
 
     app.listen(PORT , () => {
         console.log(`server started at ${PORT}`);
+
     })
 }
 
